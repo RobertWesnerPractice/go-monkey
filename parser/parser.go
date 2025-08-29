2eccd72f414a9a631f1966db4edfbd34a6c35788
Robert Wesner <robert@wesner.io>
2025-08-29 20:46:11 +0200

    implemented partial return statement parsing

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
@@ -10,14 +11,26 @@ type Parser struct {
 	l            *lexer.Lexer
 	tokenCurrent token.Token
 	tokenPeek    token.Token
+	errors       []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.Type) {
+	p.errors = append(
+		p.errors,
+		fmt.Sprintf("expected next token to be %v, got %v instead", t.Debug(), p.tokenPeek.Type.Debug()),
+	)
 }
 
 func (p *Parser) nextToken() {
@@ -29,7 +42,7 @@ func (p *Parser) Parse() *ast.Program {
 	program := &ast.Program{}
 	program.Statements = []ast.Statement{}
 
-	for p.tokenCurrent.Type != token.EOF {
+	for !p.currentIs(token.EOF) {
 		stmt := p.parseStatement()
 		if stmt != nil {
 			program.Statements = append(program.Statements, stmt)
@@ -44,6 +57,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.tokenCurrent.Type {
 	case token.Let:
 		return p.parseLetStatement()
+	case token.Return:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -70,6 +85,19 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	return stmt
 }
 
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.tokenCurrent}
+
+	p.nextToken()
+
+	// TODO: We're skipping the expressinos until we envounter a semicolon
+	for !p.currentIs(token.Semicolon) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
 func (p *Parser) currentIs(t token.Type) bool {
 	return p.tokenCurrent.Type == t
 }
@@ -84,6 +112,8 @@ func (p *Parser) expectPeek(t token.Type) bool {
 
 		return true
 	}
+
+	p.peekError(t)
 
 	return false
 }
